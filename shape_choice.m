af5410cdeb6f58217fd849be751a3c8c5952f737
FValo <fvaloriani@gmail.com>
2021-10-11 15:49:16 +0200

    add shape_choice, chose the best modal shape for modal method

---
+++ shape_choice.m
@@ -0,0 +1,42 @@
+function [num_modi_usare] = shape_choice(ms_str,def,omega)
+% criterio di selezione dei modi
+%_______________________________________________________
+% ms_str: matrice delle forme modali in termini di deformazioni
+% def: vettore colonna contenente le deformazioni misurate
+% omega: pulsazioni dei modi
+%_______________________________________________________
+
+q=(ms_str'*ms_str) \ ms_str' * def;
+
+Ei=1/2*omega.^2.*q.^2;
+E_tot=sum(Ei);
+
+num_modi=zeros(size(Ei,1)+1,1);
+E_accumulata=0;
+i=1;
+
+while true
+    massimo=max(Ei);
+    num_modi(i)=find(Ei==massimo);
+    
+    E_accumulata=E_accumulata+massimo;
+    Ei(num_modi(i))=0;
+    
+    if E_accumulata/E_tot>0.96
+        break
+    else
+        i=i+1;
+    end
+       
+end
+
+% forzare inserimento ultimo modo
+num_modi(end)=size(Ei,1);
+num_modi_usare=unique(num_modi);
+
+if num_modi_usare(1)==0
+    num_modi_usare(1)=[];
+end
+
+end
+
